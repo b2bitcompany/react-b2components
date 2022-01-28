ab8b1cec1d1b67f580d91f5e36ca7bbbce182446
viniciussslima <viniciussslima999@gmail.com>
2022-01-28 15:46:17 -0300

    Remove default comments

--- stories/B2Button.stories.tsx
+++ stories/B2Button.stories.tsx
@@ -32,8 +32,6 @@ const Template: Story<IB2Button> = (args) => (
   </B2Theme>
 );
 
-// By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
-// https://storybook.js.org/docs/react/workflows/unit-testing
 export const Default = Template.bind({});
 export const Secondary = Template.bind({});
 export const Outline = Template.bind({});
--- stories/B2ButtonsPage.stories.tsx
+++ stories/B2ButtonsPage.stories.tsx
@@ -28,6 +28,4 @@ const Template: Story<IB2ButtonsPage> = (args) => (
   </B2Theme>
 );
 
-// By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
-// https://storybook.js.org/docs/react/workflows/unit-testing
 export const Default = Template.bind({});
