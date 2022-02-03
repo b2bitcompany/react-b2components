4a4038797e0d704c6d4b71ccce393f906a7cc296
viniciussslima <viniciussslima999@gmail.com>
2022-02-03 14:39:17 -0300

    Add theme into storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,11 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
+  addons: [
+    '@storybook/theming',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+  ],
   typescript: {
-    check: true, // type-check stories during Storybook build
-  }
+    check: true,
+  },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,8 @@
-// https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
+import theme from './theme';
+
 export const parameters = {
-  // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
   actions: { argTypesRegex: '^on.*' },
+  docs: {
+    theme: theme,
+  },
 };
