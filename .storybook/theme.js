93bd5c933cf0a62d3f779386a37aab8809d5a3f7
viniciussslima <viniciussslima999@gmail.com>
2022-02-03 15:06:01 -0300

    Adjust theme

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -4,11 +4,10 @@ export default create({
   base: 'dark',
   brandTitle: 'React-b2components',
   brandUrl: 'https://b2bit.company',
-  brandImage:
-    'https://media-exp1.licdn.com/dms/image/C4D0BAQGAiAsexYztpA/company-logo_200_200/0/1613565854242?e=2159024400&v=beta&t=HQqVBv3U_r8v7KIx4Mp5U93xVykZGYiFOvkGtlZpfJM',
+  brandImage: 'https://i.imgur.com/CYmC8ln.png',
   colorPrimary: '#02274F',
-  colorSecondary: '#FDCF00',
-  appBg: '#343540',
-  barBg: '#343540',
-  appContentBg: '#343540',
+  colorSecondary: '#B1B1B4',
+  appBg: '#2A2A33',
+  barBg: '#2A2A33',
+  appContentBg: '#2A2A33',
 });
