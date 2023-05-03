fbe52328476a945887246dacab88d569cb7843d5
viniciussslima <viniciussslima999@gmail.com>
2023-05-03 10:41:33 -0300

    build: :package: update all packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,14 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
-  addons: [
-    '@storybook/theming',
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-  ],
+  addons: ['@storybook/theming', '@storybook/addon-links', '@storybook/addon-essentials'],
   typescript: {
-    check: true,
+    check: true
   },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
